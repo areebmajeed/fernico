6f489a963e92685c589779aca24141b807c47f87
Areeb <areebmajeed@users.noreply.github.com>
2017-02-24 19:40:36 +0530

    Added a controller to handle the fatal errors.
    Added a controller to handle the fatal errors.

---
+++ controllers/fatalErrorController.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * Fernico - Ridiculously lite PHP framework
+ *
+ * @author Areeb Majeed, Volcrado Holdings
+ * @package Fernico
+ * @copyright 2017 - Volcrado Holdings Limited
+ * @license https://opensource.org/licenses/MIT MIT License
+ * @link https://volcrado.com/
+ *
+ */
+
+if (!defined('FERNICO')) {
+    fernico_destroy();
+}
+
+class fatalErrorController extends AstridController {
+
+    public function errorHandler() {
+        fernico_loadComponent('fatalError', 'error.tpl', array('message' => $err_text, 'error_message' => $error_msg));
+    }
+
+}
