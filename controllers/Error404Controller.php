cfd7f0346adc6c7b199e57fddbc5b555235cb5b1
Areeb <areebmajeed@users.noreply.github.com>
2017-03-10 15:51:44 +0530

    Separating error 404 from default views

--- controllers/Error404Controller.php
+++ controllers/Error404Controller.php
@@ -19,7 +19,7 @@ class Error404Controller extends AstridController {
 
     public function error404() {
         http_response_code(404);
-        $this->renderTemplate('Error404.tpl');
+        fernico_loadComponent('notFoundError', 'error.tpl', array('message' => $err_text, 'error_message' => $error_msg));
     }
 
 }
